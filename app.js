6feb38fb89448ad14f71e1d3b2e884fde5022862
happygod119 <happ0983493655@gmail.com>
2022-05-03 13:26:43 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const { getUser } = require('./helpers/auth-helpers')
-
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -39,6 +38,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
